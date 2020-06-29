f8169233fe08b4e0faee2306a6ab3d4f4e662ca7
RafaCruzA <48972341+RafaCruzA@users.noreply.github.com>
2020-06-29 12:36:04 -0600

    controller

--- modelo/src/main/java/com/uca/capas/modelo/controller/Laboratorio8Controller.java
+++ modelo/src/main/java/com/uca/capas/modelo/controller/Laboratorio8Controller.java
@@ -1,17 +1,67 @@
 package com.uca.capas.modelo.controller;
 
+import java.util.logging.Logger;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.uca.capas.modelo.domain.Cliente;
+import com.uca.capas.modelo.service.ClienteService;
 
 @Controller
 public class Laboratorio8Controller {
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	private static final Logger logger = Logger.getLogger(Laboratorio8Controller.class.getName());
+	
 
 	//MENU PRINCIPAL LABORATORIO 8
 	@RequestMapping("/indexLaboratorio8")
 	public ModelAndView indexLaboratorio8() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("Laboratorio/indexLaboratorio8");
+		return mav;
+	}
+	
+	//INSERTAR CLIENTE
+	@RequestMapping("/insertcliente")
+	public ModelAndView nuevoCliente() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("cliente", new Cliente());
+		mav.setViewName("Laboratorio/agregarCliente");
+		return mav;
+	}
+	
+	@RequestMapping(value="/savecliente", method = RequestMethod.POST)
+	public ModelAndView saveCliente(@Valid @ModelAttribute("cliente") Cliente c, BindingResult r) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("cliente", new Cliente());
+		if(r.hasErrors()) {
+			mav.addObject("resultado", 0);
+			mav.setViewName("Laboratorio/agregarCliente");
+		}
+		else {
+			Integer key = null;
+			if(c.getCcliente() == null) {//Insert
+				mav.addObject("resultado", 1);
+				key = clienteService.insertClienteAutoId(c);
+			}
+			else {//Update
+				mav.addObject("resultado", 1);
+				clienteService.updateCliente(c);
+			}
+			mav.addObject("resultado", key);
+			mav.setViewName("Laboratorio/indexLaboratorio8");
+		}
 		return mav;
 	}
 }
